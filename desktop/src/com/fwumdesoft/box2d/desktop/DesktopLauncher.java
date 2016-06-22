5333b9aa2f52e207b8edc4612d61cf87ecee515e
Jason Carrete <jasoncarrete5@gmail.com>
2016-06-21 22:53:46 -0400

    Added libgdx framework

---
+++ desktop/src/com/fwumdesoft/box2d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fwumdesoft.box2d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fwumdesoft.box2d.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
