815e3c6b064ba4b1a96bc5092606f2336cca939b
Jason Carrete <jasoncarrete5@gmail.com>
2016-06-21 23:03:04 -0400

    Setup desktop sonfig

--- desktop/src/com/fwumdesoft/box2d/desktop/DesktopLauncher.java
+++ desktop/src/com/fwumdesoft/box2d/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.fwumdesoft.box2d.Main;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "box2d-game";
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new Main(), config);
 	}
 }
