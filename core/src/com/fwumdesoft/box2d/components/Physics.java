2cd68d647c9aa98ff7bc7e8deaa952ea07f19f7c
Jason Carrete <jasoncarrete5@gmail.com>
2016-06-23 21:39:07 -0400

    Added physics system

---
+++ core/src/com/fwumdesoft/box2d/components/Physics.java
@@ -0,0 +1,16 @@
+package com.fwumdesoft.box2d.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class Physics implements Component {
+	private Body body;
+	
+	public Body getBody() {
+		return body;
+	}
+	
+	public void setBody(Body body) {
+		this.body = body;
+	}
+}
---
+++ core/src/com/fwumdesoft/box2d/systems/PhysicsSystem.java
@@ -0,0 +1,41 @@
+package com.fwumdesoft.box2d.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IntervalIteratingSystem;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Transform;
+import com.badlogic.gdx.physics.box2d.World;
+import com.fwumdesoft.box2d.components.Physics;
+
+public class PhysicsSystem extends IntervalIteratingSystem {
+	public static final float TIME_STEP = 1 / 60f;
+	
+	private static ComponentMapper<Physics> physicsMapper = ComponentMapper.getFor(Physics.class);
+	private World world;
+	
+	public PhysicsSystem(World world) {
+		super(Family.all(Physics.class).get(), TIME_STEP);
+		this.world = world;
+	}
+
+	@Override
+	protected void updateInterval() {
+		world.step(TIME_STEP, 6, 2);
+		super.updateInterval();
+	}
+	
+	@Override
+	protected void processEntity(Entity entity) {
+		Physics physics = physicsMapper.get(entity);
+		if(physics.getBody().getUserData() instanceof Sprite) {
+			//update sprite information
+			Sprite sprite = (Sprite)physics.getBody().getUserData();
+			Transform transform = physics.getBody().getTransform();
+			sprite.setPosition(transform.getPosition().x, transform.getPosition().y);
+			sprite.setRotation(transform.getRotation() * MathUtils.radDeg);
+		}
+	}
+}
