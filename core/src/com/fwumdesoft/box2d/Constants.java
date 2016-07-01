b92ab22a18aab26d4b84d05eda08faf528b51ed5
Jason Carrete <jasoncarrete5@gmail.com>
2016-06-30 22:43:01 -0400

    Removed MAX_PLAYER_ANGULAR_SPEED constant

--- core/src/com/fwumdesoft/box2d/Constants.java
+++ core/src/com/fwumdesoft/box2d/Constants.java
@@ -1,11 +1,8 @@
 package com.fwumdesoft.box2d;
 
-import com.badlogic.gdx.math.MathUtils;
-
 /**
  * Contains various constants.
  */
 public class Constants {
 	public static final float MAX_PLAYER_SPEED = 3f; // m/s
-	public static final float MAX_PLAYER_ANGULAR_SPEED = MathUtils.PI; // rad/s
 }
